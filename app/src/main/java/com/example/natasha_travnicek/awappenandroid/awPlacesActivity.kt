74837a80c945b1b1870eb16b215fb76f6b6676ee
Per <erikperjan@gmail.com>
2018-12-18 17:16:49 +0100

    Hoppas det här funkar! Fragment bottom_navigation  - Home  - Search  - Favoriter

--- app/src/main/java/com/example/natasha_travnicek/awappenandroid/awPlacesActivity.kt
+++ app/src/main/java/com/example/natasha_travnicek/awappenandroid/awPlacesActivity.kt
@@ -1,15 +1,27 @@
 package com.example.natasha_travnicek.awappenandroid
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_aw_places.*
 
+
 class awPlacesActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aw_places)
+
+
+
+       // bottom_navigation.selectedItemId = R.id.nav_search
+
+     //  bottom_navigation.setOnNavigationItemSelectedListener(m0nNavigationItemSelectedListener)
+
+
 
         awPlacesRecView.layoutManager = LinearLayoutManager(this)
 
@@ -17,6 +29,10 @@ class awPlacesActivity : AppCompatActivity() {
 
     }
 
+    fun goToOther(){
+        val goTobla = Intent(this, mainActivity::class.java)
+        startActivity(goTobla)
+    }
 
 
 }
--- app/src/main/java/com/example/natasha_travnicek/awappenandroid/mainActivity.kt
+++ app/src/main/java/com/example/natasha_travnicek/awappenandroid/mainActivity.kt
@@ -1,28 +1,102 @@
 package com.example.natasha_travnicek.awappenandroid
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
-class mainActivity : AppCompatActivity() {
+class mainActivity : AppCompatActivity(), HomeFragment.OnFragmentInteractionListener, SearchFragment.OnFragmentInteractionListener, FavoriteFragment.OnFragmentInteractionListener{
+
+
+
+    private val m0nNavigationItemSelectedListener =  BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId) {
+
+            R.id.Home_Container -> {
+                Log.d("TAG", "Hem")
+
+               replaceFragmentHome(HomeFragment())
+
+
+                return@OnNavigationItemSelectedListener true
+            }
+
+        R.id.Search_Container -> {
+            Log.d("TAG", "sök")
+
+            replaceFragmentSearch(SearchFragment())
+           // bottom_navigation.selectedItemId = R.id.nav_search
+
+              return@OnNavigationItemSelectedListener true
+
+        }
+        R.id.Favorite_Container -> {
+
+            replaceFragmentFavorite(FavoriteFragment())
+
+            return@OnNavigationItemSelectedListener true
+        }
+    }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        //recyclerView_Search.layoutManager = LinearLayoutManager(this)
+
+        //recyclerView_Search.adapter = awPlacesAdapter()
+
+
+
+bottom_navigation.setOnNavigationItemSelectedListener(m0nNavigationItemSelectedListener)
+
+
+      replaceFragmentHome(HomeFragment())
+
+    //  replaceFragmentHome(HomeFragment())
+    //  bottom_navigation.selectedItemId = R.id.Search_Container
+
+
+
+
+
     }
 
-    fun toAWPlaces(view: View){
-
-        val goToOtherIntent = Intent(this, awPlacesActivity::class.java)
-
-        startActivity(goToOtherIntent)
 
 
 
+
+    private fun replaceFragmentHome(fragment: Fragment){
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_Container, HomeFragment())
+      //  transaction.addToBackStack(null)
+        transaction.commit()
     }
 
+
+    private fun replaceFragmentSearch(fragment: Fragment){
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_Container, SearchFragment())
+        //  transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun replaceFragmentFavorite(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_Container, FavoriteFragment())
+        transaction.commit()
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
