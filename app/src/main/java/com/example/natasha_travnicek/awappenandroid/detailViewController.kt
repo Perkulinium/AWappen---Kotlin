1123dd4d32aa9dba85b3c5fe7f8692267d6898f0
natashatrav <travixar@gmail.com>
2019-01-13 17:49:27 +0100

    added map and photos

--- app/src/main/java/com/example/natasha_travnicek/awappenandroid/detailViewController.kt
+++ app/src/main/java/com/example/natasha_travnicek/awappenandroid/detailViewController.kt
@@ -1,7 +1,9 @@
 package com.example.natasha_travnicek.awappenandroid
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import kotlinx.android.synthetic.main.activity_detail_view_controller.*
 
 class detailViewController : AppCompatActivity() {
@@ -12,6 +14,16 @@ class detailViewController : AppCompatActivity() {
 
 
         textTextView.text = intent.getStringExtra("awPlaceTextView1")
+
+
+    }
+
+    fun toMapTemp(view: View){
+
+        val goToOtherIntent = Intent(this, MapAndDetailsMapsActivity::class.java)
+
+        startActivity(goToOtherIntent)
+
 
 
     }
