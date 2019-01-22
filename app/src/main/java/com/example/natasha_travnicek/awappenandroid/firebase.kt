a4778f924cebdc72758c75328f410c6b5221ff2c
natashatrav <travixar@gmail.com>
2019-01-22 12:43:08 +0100

    marker on map fixed

--- app/src/main/java/com/example/natasha_travnicek/awappenandroid/firebase.kt
+++ app/src/main/java/com/example/natasha_travnicek/awappenandroid/firebase.kt
@@ -3,7 +3,6 @@ package com.example.natasha_travnicek.awappenandroid
 import java.io.Serializable
 
 data class firebase (var fbKey : String? = null,
-                    var name : String? = "",
                     var Longitude : Double? = null,
                     var Latitude : Double? = null,
                     var Openinghours : String? = "",
